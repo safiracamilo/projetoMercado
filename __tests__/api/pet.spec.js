c0723d6a6eb8740d1368d4c216ff5a2c8ae74182
safiracamilo <safiracamilo@email.com>
2022-12-01 22:47:42 +0000

    Inclusao da Funcao carga e delete

--- __tests__/api/pet.spec.js
+++ __tests__/api/pet.spec.js
@@ -10,10 +10,33 @@ const petId = 5191985; // codigo do animal
 
 describe("PetStore Swagger - Pet", () => {
     const request = supertest(baseUrl);
+    const pets = require("../../vendors/json/petn");
+    let pet = require("../../vendors/json/petBase.json");
+
+    pets.array.forEach(({nomePet, idPet, nomeCategoria, idCategoria}) =>{
+        it("Setup Swagger - Add Pets" + nomePet, ()=> {
+            pet.id = idPet
+            pet.name = nomePet
+            pet.category.id = idCategoria
+            pet.category.name = nomeCategoria
+            pet.tags[0].id = 3
+            pet.tags[0].name = "vaccined"
+            pet.status = "done"
+
+            return request
+                .post("/pet")
+                .send(pet)
+                .then((response) => {
+                    assert.equal(response.statusCode, 200)
+                })
+
+        });//Fecha o it
+
+    }) 
 
     // Post - Teste de incluir um animal
     it("Post Pet", () => {
-        // Configura
+        // ConfiguraS
         // Apontou para o arquivo com os dados do animal        
         const jsonFile = require("../../vendors/json/pet1");
         // Realizar a requisição e receber a resposta
@@ -67,9 +90,24 @@ describe("PetStore Swagger - Pet", () => {
             .delete("/pet/" + petId)
             .then((response) => {
                 assert.equal(response.statusCode,200)
-            });
-    });
+            });//Fecha then
+    });//Fecha o It
 
+    //Função de carga de animais -Setup
+    pets.array.forEach(({nomePet, idPet}) =>{      
+        
+        it('Teardown Swagger - Delete Pets -' + nomePet, () => {
+        
+            return request
+                .delete(`/pet/${idPet}`)
+                .then((response) => {
+                    assert.equal(response.statusCode, 200)
+            })
+        })
+
+    });//Fecha o forEach     
+
+    
 }); // fecha o describe
 
 
---
+++ vendors/json/petn.js
@@ -0,0 +1,29 @@
+module.exports.array = [
+    {
+        "nomePet": "Amora",
+        "idPet": "10",
+        "nomeCategoria": "Dog",
+        "idCategoria": "1",
+    },
+    
+    {
+        "nomePet": "Mel",
+        "idPet": "11",
+        "nomeCategoria": "Dog",
+        "idCategoria": "1",
+    },
+    
+    {
+        "nomePet": "Fox",
+        "idPet": "12",
+        "nomeCategoria": "Cat",
+        "idCategoria": "2",
+    },
+
+    {
+        "nomePet": "Lis",
+        "idPet": "13",
+        "nomeCategoria": "Cat",
+        "idCategoria": "2",
+    }      
+]
